3dc832f69b86c478d1fb45fa586d61d0c47e67cf
Trevor White <trevor.white@wayne.edu>
2022-02-09 18:14:21 -0500

    Initial tweetdeck version upload

--- monitor/twittertoolkit.py
+++ monitor/twittertoolkit.py
@@ -24,20 +24,20 @@ def locate_all_on_screen_and_click(image, wait=0.05, confidence=0.9):
         return False
 
 
-def get_notifications():
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\notification.png')
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\notification2.png',
-                                   confidence=0.95)
+# def get_notifications():
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\notification.png')
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\notification2.png',
+#                                    confidence=0.95)
+#
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\all_bold.png')
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\all.png')
+#     sleep(5)
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\mentions_bold.png')
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\mentions.png')
 
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\all_bold.png')
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\all.png')
-    sleep(5)
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\mentions_bold.png')
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\mentions.png')
 
-
-def get_messages():
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\message.png')
+# def get_messages():
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\message.png')
 
 
 # def get_home():
@@ -46,18 +46,27 @@ def get_messages():
 #     if not homes:
 #         locate_all_on_screen_and_click(r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\home2.png', confidence=0.95)
 
-def get_mentions_screen():
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\notification.png')
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\mentions.png')
+# def get_mentions_screen():
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\notification.png')
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\mentions.png')
+
+
+# def get_terminals():
+#     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\terminal.png',
+#                                    confidence=0.97)
+
+def get_refresh_mentions():
+    locate_all_on_screen_and_click(
+        image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\refresh_mentions.png')
+
+
+def get_mark_all_as_read():
+    locate_all_on_screen_and_click(
+        image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\mark_all_as_read.png')
 
 
 def get_refresh():
     locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\refresh.png')
-
-
-def get_terminals():
-    locate_all_on_screen_and_click(image=r'C:\Users\trevo\Documents\GitHub\BotTools\monitor\assets\terminal.png',
-                                   confidence=0.97)
 
 
 def get_exit():
@@ -65,8 +74,8 @@ def get_exit():
 
 
 def get_next_pi():
-    get_mentions_screen()
-    get_terminals()
+    get_refresh_mentions()
+    get_mark_all_as_read()
     get_exit()
 
 
@@ -119,11 +128,9 @@ def center_window(window, width, height):
 def run_gui():
     options = {
         "Next Pi": get_next_pi,
-        "Notifications": get_notifications,
-        "Messages": get_messages,
-        "Mentions": get_mentions_screen,
+        "Mentions": get_refresh_mentions,
+        "Mark All DM's Read": get_mark_all_as_read,
         "Refresh": get_refresh,
-        "Terminals": get_terminals,
         "Exit Pi": get_exit,
         "Locked Tweet Reply": lambda: copy_reply_text(type="locked_tweet"),
         "Tweet Reply": lambda: copy_reply_text(type="tweet"),
@@ -132,7 +139,7 @@ def run_gui():
     options.update(get_crypto_buttons())
     buttons = {}
     window_width = 265
-    window_height = 951
+    window_height = 952
     button_width = 18
     button_height = 3
     window = tk.Tk()
@@ -141,17 +148,16 @@ def run_gui():
     window.geometry(f'{window_width}x{window_height}')
 
     for index, option in enumerate(options.keys()):
-        if index < 3:
+        if index == 0:
             buttons[option] = tk.Button(window, text=option, height=button_height, width=button_width,
                                         command=options.get(option), bg='#FAD7A0').grid(columnspan=2, row=index,
                                                                                         column=0, sticky='nesw')
-        elif 2 < index < 5:
-            buttons[option] = tk.Button(window, text=option, height=button_height, width=int(button_width),
-                                        command=options.get(option), bg='#CCD1D1').grid(row=3, column=index - 3)
-        elif 4 < index < 7:
-            buttons[option] = tk.Button(window, text=option, height=button_height, width=int(button_width),
-                                        command=options.get(option), bg='#CCD1D1').grid(row=4, column=index - 5)
-        elif index == 7 or index == 8 or index == 9:
+        elif 0 < index < 5:
+            buttons[option] = tk.Button(window, text=option, height=button_height, width=button_width,
+                                        command=options.get(option), bg='#CCD1D1').grid(columnspan=2, row=index,
+                                                                                        column=0, sticky='nesw')
+
+        elif 4 < index < 8:
             buttons[option] = tk.Button(window, text=option, height=button_height, width=button_width,
                                         command=options.get(option), bg='#5499C7').grid(columnspan=2, row=index,
                                                                                         column=0, sticky='nesw')
